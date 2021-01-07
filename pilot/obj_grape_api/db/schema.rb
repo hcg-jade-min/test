44657c6091caf3f0f2e5a56e9ca481b5221bcf5f
hcg-jade-min <hcgjademin@gmail.com>
2021-01-07 10:49:02 +0900

    ppobjback: test

--- pilot/obj_grape_api/db/schema.rb
+++ pilot/obj_grape_api/db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_053409) do
+ActiveRecord::Schema.define(version: 2020_12_30_074635) do
+
+  create_table "abcs", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "key_results", force: :cascade do |t|
     t.string "name"
