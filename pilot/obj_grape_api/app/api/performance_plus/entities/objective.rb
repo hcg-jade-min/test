800995791a37fb489cb5913ce8d9163ad6b93183
hcg-jade-min <hcgjademin@gmail.com>
2021-01-07 10:39:11 +0900

    ppobjback: API 동작시 response body 에 id 값 나오도록 설정

--- pilot/obj_grape_api/app/api/performance_plus/entities/objective.rb
+++ pilot/obj_grape_api/app/api/performance_plus/entities/objective.rb
@@ -1,10 +1,11 @@
 module PerformancePlus
   module Entities
     class Objective < Grape::Entity
+      expose :id
       expose :name
       expose :description
       expose :started_on
-      expose :started_on
+      expose :ended_on
       expose :status
       expose :achievement
       expose :created_at
