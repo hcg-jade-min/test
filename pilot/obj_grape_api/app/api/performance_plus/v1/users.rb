6b6d9f898123456c5c416eaa3e2f9506d0fb3644
hcg-jade-min <hcgjademin@gmail.com>
2021-01-12 11:02:07 +0900

    ppobjback: grape api 의 User 로직에 쿠키 활용 추가

--- pilot/obj_grape_api/app/api/performance_plus/v1/users.rb
+++ pilot/obj_grape_api/app/api/performance_plus/v1/users.rb
@@ -7,23 +7,35 @@ module PerformancePlus
 
       # User : index - 모든 유저의 리스트
       resource :users do
-        desc 'Return list of users'
+        desc '유저 목록 조회' do
+          success PerformancePlus::Entities::User
+          failure [
+            { code: 400, message: 'Bad Request', model: PerformancePlus::Entities::ApiError },
+            { code: 404, message: 'Not Found', model: PerformancePlus::Entities::ApiError }
+          ]
+        end
         get do
           users = User.all
           present users, with: PerformancePlus::Entities::User
         end
         ###
         # User : show - 특정한 유저
-        desc 'Return a specific user'
-        route_param :id do
-          get do
-            user = User.find(params[:id])
-            present user, with: PerformancePlus::Entities::User
-          end
-        end
+        # desc 'Return a specific user'
+        # route_param :id do
+        #   get do
+        #     user = User.find(params[:id])
+        #     present user, with: PerformancePlus::Entities::User
+        #   end
+        # end
         ###
         # User : create - 새로운 유저 생성
-        desc 'Create New User'
+        desc '유저 생성' do
+          success PerformancePlus::Entities::User
+          failure [
+            { code: 400, message: 'Bad request', model: PerformancePlus::Entities::ApiError },
+            { code: 404, message: 'Not Found', model: PerformancePlus::Entities::ApiError }
+          ]
+        end
         params do
           requires :user, type: Hash do
             requires :username, type: String, desc: 'Username Of New User.'
@@ -31,41 +43,54 @@ module PerformancePlus
           end
         end
         post do
-          @user = User.create!(params[:user])
+          # @user = User.create!(params[:user])
+          @user = User.new(params[:user])
+          cookies[:user_id] = @user.id if @user.save
         end
         # User : Login - 아이디, 패스워드 맞으면 통과
-        desc 'Login User'
-        params do
-          requires :user, type: Hash do
-            requires :username, type: String, desc: 'Check The Username Of User To Login'
-            requires :password, type: String, desc: 'Check The Password Of User To Login'
-          end
+        desc '유저 로그인' do
+          success PerformancePlus::Entities::User
+          failure [
+            { code: 400, message: 'Bad request', model: PerformancePlus::Entities::ApiError },
+            { code: 404, message: 'Not Found', model: PerformancePlus::Entities::ApiError }
+          ]
         end
-        post 'login' do
-          username_to_check = params[:user]['username']
-          password_to_check = params[:user]['password']
-          if User.where('username' => (params[:user]['username']).to_s)
-            username_in_db = User.where('username' => (params[:user]['username']).to_s)[0]['username']
-            password_in_db = User.where('username' => (params[:user]['username']).to_s)[0]['password']
-          else
-            present 'There is no Username'
-          end
-          if username_to_check == username_in_db && password_to_check == password_in_db
-            present 'Login Success'
-          else
-            present 'Login Failed'
-          end
-          #   present user_to_check[0]['password'] if user_to_check && (user_to_check[0])
-          #   Client.where('locked' => true)
-          #   Client.where("orders_count = #{params[:orders]}")
-          #   Client.where("orders_count = ?", params[:orders])
-          #   present 'ok'
-          #   user_to_check = User.find(params[:user])
-          #   if user_to_check.username == @user.username && user_to_check.password == @user.password
-          #     present true
-          #   else
-          #     present false
-          #   end
+        params do
+          # requires :user, type: Hash do
+          requires :username, type: String, desc: 'Check The Username Of User To Login'
+          requires :password, type: String, desc: 'Check The Password Of User To Login'
+          # end
+        end
+        post '/login' do
+          user = User.find_by!(username: params[:username],
+                               password: params[:password])
+
+          cookies[:user_id] = {
+            value: user.id,
+            expires: Time.now + 1.day,
+            domain: 'localhost',
+            path: '/'
+          }
+        end
+
+        desc '유저 로그아웃' do
+          success PerformancePlus::Entities::User
+          failure [
+            { code: 400, message: 'Bad request', model: PerformancePlus::Entities::ApiError },
+            { code: 404, message: 'Not Found', model: PerformancePlus::Entities::ApiError }
+          ]
+        end
+        params do
+          # requires :user, type: Hash do
+          requires :username, type: String, desc: 'Check The Username Of User To Login'
+          requires :password, type: String, desc: 'Check The Password Of User To Login'
+          # end
+        end
+        post '/logout' do
+          user = User.find_by!(username: params[:username],
+                               password: params[:password])
+
+          cookies.delete :user, path: '/'
         end
       end # end of return a specific user
     end
